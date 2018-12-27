7baa259f3fd2a5859e4afcad359973c65524d6a7
NJ <r_curtain@163.com>
2018-12-27 17:23:14 +0800

    4.使用creat-react-app来创建了101，102项目，学习使用了react-router-dom

---
+++ 102-reactrouter/src/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <a href='#/detail'>去detail</a>
+                <button onClick={() => this.props.history.push('detail')}>通过函数跳转</button>
+            </div>
+        )
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LearnReact
-参照教程https://react.docschina.org/docs
+0××- 开头的文件夹  参照教程https://react.docschina.org/docs
+1××- 开头的文件夹  是使用create-react-app创建的app项目
